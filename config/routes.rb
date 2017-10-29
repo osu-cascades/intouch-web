8f8aa59b02fa359c4c65a81d5a97c4f08975c9a8
Aaronmb123 <bakeaaro@oregonstate.edu>
2017-10-29 14:49:12 -0700

    7.12. a users resource

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   #get 'static_pages/groups'
   get '/groups' , to: 'static_pages#groups'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  resources :users
   
 
 end
