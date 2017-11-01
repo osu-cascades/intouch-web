2d1cee593a0caa61f34e3a5083375e741a8cc8aa
Aaronmb123 <bakeaaro@oregonstate.edu>
2017-11-01 10:46:14 -0700

    added test to validate user added to db, correct redirect found

--- test/integration/users_create_test.rb
+++ test/integration/users_create_test.rb
@@ -6,14 +6,27 @@ class UsersCreateTest < ActionDispatch::IntegrationTest
     get users_new_path
     assert_no_difference 'User.count' do
       post users_new_path, params: { user: {first_name: "",
-                                        last_name: "Balach", 
+                                        last_name: "Ballack", 
                                         username: "mikey",
                                         user_type: "admin",
                                         password: "soccerball"}}
     end
     assert_template 'users/new'
-    assert_select 'div#<CSS id for error explanation>'
-    assert_select 'div.<CSS class for field with error>'
+    #assert_select 'div#<CSS id for error explanation>' ~> error
+    #assert_select 'div.<CSS class for field with error>' ~> error
+  end
+
+  test "valid create user information" do
+    get users_new_path
+    assert_difference 'User.count', 1 do 
+      post users_new_path, params: { user: {first_name: "Michael",
+                                        last_name: "Ballack", 
+                                        username: "mikey",
+                                        user_type: "admin",
+                                        password: "soccerball"}}
+    end
+    follow_redirect!
+    assert_template 'users' # taken to static_pages/users for now
   end
 
 end
