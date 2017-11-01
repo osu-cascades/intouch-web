4908751c13d3b1ad4d6577c80f5774328e0ac136
Aaronmb123 <bakeaaro@oregonstate.edu>
2017-10-31 18:11:21 -0700

    adding error checking to create user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,11 +9,17 @@ class UsersController < ApplicationController
   end
 
   def create
-  	@user = User.new(params[:user])
+  	@user = User.new(user_params)
   	if @user.save
   	else
   		render 'new'
   	end
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :user_type, :username, :password)
+    end
 
 end
