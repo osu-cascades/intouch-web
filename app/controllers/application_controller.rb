3352b6b33e43a517f0b7cb1210f9d0253bd368da
Aaronmb123 <bakeaaro@oregonstate.edu>
2017-11-02 18:59:17 -0700

    session helper to get current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
-	def log_in(user)
-		session[:user_id] = user.id
-	end
+	
 
 end
