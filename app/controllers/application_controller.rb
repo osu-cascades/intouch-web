ef3a66167165c2311983584c7767d3b13df30bfb
Aaronmb123 <bakeaaro@oregonstate.edu>
2017-11-02 18:55:05 -0700

    valid login takes user to notifications screen

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include SessionsHelper
+
+	def log_in(user)
+		session[:user_id] = user.id
+	end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
   def create
   	user = User.find_by(username: params[:session][:username].downcase)
   	if user && user.authenticate(params[:session][:password])
-
+      log_in user
+      redirect_to notifications_url
   	else
       flash.now[:danger] = 'Invalid username and/or password'
   		render 'new'
