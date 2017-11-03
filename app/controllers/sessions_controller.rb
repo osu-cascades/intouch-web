528470c0399e09cbad731d875419c88de88f9ec7
Aaronmb123 <bakeaaro@oregonstate.edu>
2017-11-02 18:38:06 -0700

    authenticate session controller logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
+  
   def new
   end
+
+  def create
+  	user = User.find_by(username: params[:session][:username].downcase)
+  	if user && user.authenticate(params[:session][:password])
+
+  	else
+      flash[:danger] = 'Invalid username and/or password'
+  		render 'new'
+  	end
+
+  end
+
+  def destroy
+  end
+
+
 end
