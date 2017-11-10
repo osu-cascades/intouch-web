aa6731704b6b4e9ca2a2485eec2abfcb6aa5e7ee
Aaronmb123 <bakeaaro@oregonstate.edu>
2017-11-09 20:21:13 -0800

    group name must be unique

--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -10,4 +10,21 @@ class GroupTest < ActiveSupport::TestCase
     assert @group.valid?
   end
 
+  test "name should be present" do
+    @group.name = "     "
+    assert_not @group.valid?
+  end
+
+  test "name should not be too long" do
+    @group.name = "a" * 101
+    assert_not @group.valid?
+  end
+
+  test "name should be unique" do
+    duplicate_group = @group.dup
+    duplicate_group.name = @group.name.upcase
+    @group.save
+    assert_not duplicate_group.valid?
+  end
+
 end
