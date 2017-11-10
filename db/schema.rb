7e94b9ad9f67267806377142068dcd0f1f3f605c
Aaronmb123 <bakeaaro@oregonstate.edu>
2017-11-09 20:09:45 -0800

    added group valid test

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171101195234) do
+ActiveRecord::Schema.define(version: 20171110035931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "notifications", force: :cascade do |t|
     t.string "title"
