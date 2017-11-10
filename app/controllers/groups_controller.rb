e44d97afb28dd267b81b49c93abae05628f85a31
Aaronmb123 <bakeaaro@oregonstate.edu>
2017-11-09 21:03:43 -0800

    create and view groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,10 +1,27 @@
 class GroupsController < ApplicationController
 
   def show
-    @group = Group.find(params[:id])
+    @groups = Group.all
   end
 
   def new
     @group = Group.new
   end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      flash[:success] = "New group created!"
+      redirect_to '/groups/show'
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def group_params
+      params.require(:group).permit(:name)
+    end
+
 end
