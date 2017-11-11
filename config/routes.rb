c4b5fa1da0f8862e333145e990050aa14022e2e8
Aaronmb123 <bakeaaro@oregonstate.edu>
2017-11-10 19:42:17 -0800

    removed unnecessary routes, using routes from resources function

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,16 @@ Rails.application.routes.draw do
   #get '/groups' , to: 'static_pages#groups'
   get '/groups', to: 'groups#show'
 
+  resources :groups
+  #             GET    /groups(.:format)                 groups#index
+  #             POST   /groups(.:format)                 groups#create
+  #  new_group  GET    /groups/new(.:format)             groups#new
+  # edit_group  GET    /groups/:id/edit(.:format)        groups#edit
+  #      group  GET    /groups/:id(.:format)             groups#show
+  #             PATCH  /groups/:id(.:format)             groups#update
+  #             PUT    /groups/:id(.:format)             groups#update
+  #             DELETE /groups/:id(.:format)             groups#destroy
+
   # Login
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
@@ -20,13 +30,22 @@ Rails.application.routes.draw do
   get 'sessions/new'
 
   # Users
-  get '/users' , to: 'static_pages#users'
-  get '/users/new', to: 'users#new'
-  get '/users/show', to: 'users#show'
-  post '/users/new', to: 'users#create'
+  #get '/users' , to: 'static_pages#users'
+  #get '/users/new', to: 'users#new'
+  #get '/users', to: 'users#show'
+  #post '/users/new', to: 'users#create'
 
   resources :users
+  #           GET    /users(.:format)                  users#index
+  #           POST   /users(.:format)                  users#create
+  #  new_user GET    /users/new(.:format)              users#new
+  # edit_user GET    /users/:id/edit(.:format)         users#edit
+  #      user GET    /users/:id(.:format)              users#show
+  #           PATCH  /users/:id(.:format)              users#update
+  #           PUT    /users/:id(.:format)              users#update
+  #           DELETE /users/:id(.:format)              users#destroy
+  
   resources :notifications
-  resources :groups
+  
 
 end
