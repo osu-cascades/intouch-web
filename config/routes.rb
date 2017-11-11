69b456a3b631eced2fd49376510d1e572a4d26be
Aaronmb123 <bakeaaro@oregonstate.edu>
2017-11-10 19:48:31 -0800

    fixed group route nameing stuff

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   
   root 'static_pages#login'
 
-  # Groups 
-  #get '/groups' , to: 'static_pages#groups'
-  get '/groups', to: 'groups#show'
-
   resources :groups
   #             GET    /groups(.:format)                 groups#index
   #             POST   /groups(.:format)                 groups#create
@@ -22,18 +18,13 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
 
   # Notifications
+  resources :notifications
   get  '/notifications' , to: 'static_pages#notifications'
   post '/notifications/new',  to: 'notifications#create'
   get 'notifications/new'
 
   # Sessions
   get 'sessions/new'
-
-  # Users
-  #get '/users' , to: 'static_pages#users'
-  #get '/users/new', to: 'users#new'
-  #get '/users', to: 'users#show'
-  #post '/users/new', to: 'users#create'
 
   resources :users
   #           GET    /users(.:format)                  users#index
@@ -45,7 +36,7 @@ Rails.application.routes.draw do
   #           PUT    /users/:id(.:format)              users#update
   #           DELETE /users/:id(.:format)              users#destroy
   
-  resources :notifications
+  
   
 
 end
