4a71b57f2e942a43eafb5389479145d877a767c8
Aaronmb123 <bakeaaro@oregonstate.edu>
2017-11-13 19:16:13 -0800

    added associations to user,group

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,7 @@
 class Group < ApplicationRecord
 
+  has_and_belongs_to_many :users
+
   validates :name, presence: true, length: { maximum: 100 }, uniqueness: { case_sensitive: false }
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+  has_and_belongs_to_many :groups
+
 	# overrides rails attributes, remove
 	# attr_accessor :firstName, :lastName, :userType, :username, :
 
