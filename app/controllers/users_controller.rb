817c52452e2f4926341d2171a5b019ed801a9fb4
Aaronmb123 <bakeaaro@oregonstate.edu>
2017-11-13 19:51:18 -0800

    small changes for checkbox stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def show
+    @users = User.all
+  end
+
   def new
   	@user = User.new
   end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ApplicationRecord
 
-  has_and_belongs_to_many :users
+  has_and_belongs_to_many :users, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 100 }, uniqueness: { case_sensitive: false }
 
