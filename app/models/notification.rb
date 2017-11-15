17c2b62a48ed7f0f009a5a1233e9b6236328440a
gonzalky7 <gonzalky@oregonstate.edu>
2017-11-15 08:23:59 -0800

    Clean up indentation.

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,8 +1,7 @@
 class Notification < ApplicationRecord
-	has_and_belongs_to_many :users
+  has_and_belongs_to_many :users
 
-
-	 validates :title, presence: true
-	 validates :first_name, presence: true
-	 validates :content, presence: true
+  validates :title, presence: true
+  validates :first_name, presence: true
+  validates :content, presence: true
 end
