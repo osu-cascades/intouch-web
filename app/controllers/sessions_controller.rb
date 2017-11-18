9a6e88d53da526bea86ee9d4a119274f7fe4226f
Aaronmb123 <bakeaaro@oregonstate.edu>
2017-11-18 06:46:31 -0800

    added session helpers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
   	user = User.find_by(username: params[:session][:username].downcase)
   	if user && user.authenticate(params[:session][:password])
       log_in user
+      remember user
       redirect_to notifications_path
   	else
       flash.now[:danger] = 'Invalid username and/or password'
