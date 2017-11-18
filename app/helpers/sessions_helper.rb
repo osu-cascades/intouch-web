4dab2d3dedabe2f5bc9ba4dc53fde8587e66ad69
Aaronmb123 <bakeaaro@oregonstate.edu>
2017-11-18 06:49:40 -0800

    added session destroy methods

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -34,7 +34,14 @@ module SessionsHelper
     end
   end
 
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
