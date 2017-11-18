b51351de9d0aa33540e4910657ab44edb55f238b
Aaronmb123 <bakeaaro@oregonstate.edu>
2017-11-18 06:15:49 -0800

    created new token method

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171114031650) do
+ActiveRecord::Schema.define(version: 20171118141241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20171114031650) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
