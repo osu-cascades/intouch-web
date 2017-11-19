89a811eae32752fd6d57e4f8a9968003856b6772
Aaronmb123 <bakeaaro@oregonstate.edu>
2017-11-18 18:04:10 -0800

    added redirect tests, not passing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
+  # before_action :correct_user
+
 	def logged_in_user
       unless logged_in?
         flash[:danger] = "Access Denied"
         redirect_to login_path
       end
     end
+
+  # def correct_user
+  #   @user = User.find(params[:id])
+  #   redirect_to(root_url) unless current_user?(@user)
+  # end
 
 end
