16bce75cfd1fa1c8da61fe87abf6c1d0a69bf62f
gonzalky7 <gonzalky@oregonstate.edu>
2017-11-19 20:38:14 -0800

    Can select multiple groups

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,5 +1,6 @@
 class Notification < ApplicationRecord
   has_and_belongs_to_many :users 
+  has_and_belongs_to_many :groups 
 
   validates :title, presence: true
   validates :content, presence: true
