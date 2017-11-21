fa08ca6c186b67dbc0cd39a83c04af261321d91b
Aaronmb123 <bakeaaro@oregonstate.edu>
2017-11-20 16:43:27 -0800

    generated user role join table, migrated

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,6 +1,6 @@
 class Role < ApplicationRecord
 
-  has_many :users, dependent: :destroy
+  has_many :users
 
   validates :name, presence: true, length: { maximum: 100 }, uniqueness: { case_sensitive: false }
 
