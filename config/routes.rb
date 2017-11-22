568d7d187c76e95056a6135ad4b00de7e7b352a8
Aaronmb123 <bakeaaro@oregonstate.edu>
2017-11-21 18:42:41 -0800

    redirect to notifications path if logged in and enter root route

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
 
   # Sessions
   #get 'sessions/new'
-  get    '/login',   to: 'sessions#new'
+  get    '/login',   to: 'sessions#new' 
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
