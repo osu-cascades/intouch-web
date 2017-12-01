645b1d1d1fa39150cb20babc707c3f7ddd1f84d7
gonzalky7 <gonzalky@oregonstate.edu>
2017-12-01 12:07:03 -0800

    Fixes #35

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,6 @@
 class NotificationsController < ApplicationController
 
-  #before_action :logged_in_user
+  before_action :logged_in_user
 
   def index
     @notifications = current_user.notifications
