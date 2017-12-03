d3c517ca5b5b4867d5af71b3a077cafc0cb88ef1
Aaronmb123 <bakeaaro@oregonstate.edu>
2017-12-03 13:27:09 -0800

    added default user to seeds.rb. fixes #33

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(first_name:  "Charlene",
+             last_name: "Weirup",
+             user_type: "admin",
+             username: "charlene",
+             password: "abilitree")
+
