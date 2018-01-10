a8e2f5e1d41c5c22a0efa07c09acd69869579dc9
Yong Bakos <ybakos@humanoriented.com>
2018-01-10 09:45:38 -0800

    Add devise. - Configured action mailer. - Add devise boilerplate installation. - Change user schema and model per devise. (Remove deprecated user columns.) References #30.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "intouch_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  # Per devise.
+  config.action_mailer.default_url_options = { host: 'abilitree-intouch.herokuapp.com' }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  devise_for :users
   resources :roles
   root 'sessions#new'
 
