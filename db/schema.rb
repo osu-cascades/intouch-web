d6fdbdbbab08da9530b72acdcf09466c48951367
Yong Bakos <ybakos@humanoriented.com>
2018-01-10 09:09:52 -0800

    Synchronize schema.rb with migrations.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171210212737) do
+ActiveRecord::Schema.define(version: 20171210220321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,8 +36,6 @@ ActiveRecord::Schema.define(version: 20171210212737) do
 
   create_table "notifications", force: :cascade do |t|
     t.string "title"
-    t.string "first_name"
-    t.string "last_name"
     t.datetime "date"
     t.text "content"
     t.datetime "created_at", null: false
