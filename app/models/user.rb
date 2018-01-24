4540baa3f6ee915e06c4f3127ccbf5d079a7ac21
Aaronmb123 <bakeaaro@oregonstate.edu>
2018-01-23 16:10:52 -0800

    cleaned up user model code

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ApplicationRecord
   has_and_belongs_to_many :groups  
 	has_and_belongs_to_many :notifications
   
+validates :username, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
+  validates :email, uniqueness: true
+
 	# Per the instructions to be able to user username as key for authentication
   #https://github.com/plataformatec/devise/wiki/How-To:-Allow-users-to-sign-in-with-something-other-than-their-email-address
   # 
@@ -20,16 +23,5 @@ class User < ApplicationRecord
   def will_save_change_to_email?
     false
   end
-
-	# validates :first_name, presence: true, length: { maximum: 50 }
-	# validates :last_name, presence: true, length: { maximum: 50 }
-	# validates :user_type, presence: true, inclusion: { in: %w(admin client staff) }
-	 validates :username, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
-	# validates :password, presence: true, length: { minimum: 6, maximum: 50 }, allow_nil: true
-
-	validates :email, uniqueness: true
-
-
-	
 
 end
