b6b2c8c95ec55bf162a9c63ee3219c95e68e0ff1
Aaronmb123 <bakeaaro@oregonstate.edu>
2018-01-23 16:08:46 -0800

    fixed password length issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,32 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
-User.create!(first_name: "admin",
-            last_name: "admin",
-            user_type: "admin",
-            username: "admin",
-            password: "queenannesrevenge",
-            email: "admin@admin.com")
 
-Role.create!(name: "Admin")
-Role.create!(name: "Client")
-Role.create!(name: "Staff")
+
+Role.create!(name: "admin")
+Role.create!(name: "client")
+Role.create!(name: "staff")
 
 case Rails.env
   when "development"
+    User.create!(first_name: "admin",
+            last_name: "admin",
+            user_type: "admin",
+            username: "admin",
+            password: "password",
+            email: "admin@admin.com")
     User.create!(first_name: "staff",
             last_name: "staff",
             user_type: "staff",
             username: "staff",
-            password: "staff",
+            password: "password",
             email: "staff@staff.com")
 
     User.create!(first_name: "client",
             last_name: "client",
             user_type: "client",
             username: "client",
-            password: "client",
+            password: "password",
             email: "client@client.com")
 
     Group.create!(name: "Group1")
@@ -39,6 +40,13 @@ case Rails.env
              username: "charlene",
              password: "abilitree",
              email: "place@holder.com")
+
+    User.create!(first_name: "admin",
+            last_name: "admin",
+            user_type: "admin",
+            username: "admin",
+            password: "queenannesrevenge",
+            email: "admin@admin.com")
 end
 
 
