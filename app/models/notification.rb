1519cdb84015e0536f21ff0d9015259bd2273cb2
Aaronmb123 <bakeaaro@oregonstate.edu>
2018-01-27 15:56:29 -0800

    removed belongs to stuff

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,12 +1,8 @@
 class Notification < ApplicationRecord
-  has_and_belongs_to_many :users 
-  has_and_belongs_to_many :groups 
-
-  belongs_to :user
-
-  #belongs_to :group
+  has_many :users 
+  has_many :groups 
 
   validates :title, presence: true
   validates :content, presence: true
-  #validates :group_id, presence: true
+
 end
