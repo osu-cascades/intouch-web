cb7084f8dba86f90a62190e47b688551cd38e556
Aaronmb123 <bakeaaro@oregonstate.edu>
2018-01-27 16:19:56 -0800

    added fkey constraint back

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,6 +1,9 @@
 class Notification < ApplicationRecord
-  has_many :users 
-  has_many :groups 
+  has_and_belongs_to_many :users 
+  has_and_belongs_to_many :groups 
+
+  belongs_to :user
+
 
   validates :title, presence: true
   validates :content, presence: true
