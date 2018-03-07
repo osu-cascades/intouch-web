42367913ae01986c3d328ace5a91781bf51996e6
Aaronmb123 <bakeaaro@oregonstate.edu>
2018-03-06 20:23:00 -0800

    added token column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180130055746) do
+ActiveRecord::Schema.define(version: 20180307042147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20180130055746) do
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
     t.boolean "deactivated"
+    t.string "token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
