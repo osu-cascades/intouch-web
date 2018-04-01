512c2e49462d55a31a0f5279b69dd054f2bc6101
Aaronmb123 <bakeaaro@oregonstate.edu>
2018-04-01 09:16:14 -0700

    push api path creates notification on website

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -22,17 +22,19 @@ class ApiController < ApplicationController
     # TODO
     # if token valid?
     # create notification
-    permit_params_auth
+    permit_params_push
     username = params[:username]
     password = params[:password]
+    title = params[:title]
+    content = params[:body]
     # title = params[:title]
     # groups = params[:groups]
     # content = params[:content]
     @user = User.find_for_authentication(username: username)
     if @user && @user.valid_password?(password)
       @notification = Notification.new
-      @notification.title = "title"
-      @notification.content = "content"
+      @notification.title = title
+      @notification.content = content
       @notification.date = Time.now
       @notification.user_id = @user.id
       if @notification.save
@@ -41,8 +43,10 @@ class ApiController < ApplicationController
         send_to_fcm
         render html: 'notification sent'
       else
-        render html: 'error'
+        render html: 'error creating notification'
       end  
+    else 
+      render html: 'invalid token'
     end
 
 
@@ -54,8 +58,8 @@ class ApiController < ApplicationController
       params.permit(:username, :password)
     end
 
-    def permit_params_notification
-      params.permit(:username, :password, :title, :groups, :content)
+    def permit_params_push
+      params.permit(:username, :password, :title, :groups, :body)
     end
 
     def send_to_ios
@@ -73,17 +77,17 @@ class ApiController < ApplicationController
         header = {'Content-Type': 'application/json', 'Authorization': 'Bearer 638FD20E88772FEA09A6CDD6497E9A0'}
         data = 
         {
-            "interests":["abilitree"],
+            "interests":["abilitree-dev"],
             "apns": {
               "aps": {
                 "alert": {
-                  "title":@notification.title,
-                  "body":@notification.content,
+                  "title": @notification.title,
+                  "body": @notification.content,
                   "from": "#{@user.first_name} #{@user.last_name}",
                   "datetime": @notification.date
                 },
-                "badge":0,
-                "sound":"default"
+                "badge": 0,
+                "sound": "default"
               }
             }
         }
@@ -110,7 +114,7 @@ class ApiController < ApplicationController
         header = {'Content-Type': 'application/json', 'Authorization': 'Bearer 638FD20E88772FEA09A6CDD6497E9A0'}
         data = 
         {
-          "interests":["abilitree"],
+          "interests":["abilitree-dev"],
           "fcm": {
             "notification": {
               "title": @notification.title,
