24a8b03c7efb264b83459f7d1222f9942801fbff
Yong Bakos <ybakos@humanoriented.com>
2018-04-25 10:00:07 -0700

    Notification: Test attributes and associations.

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,11 +1,10 @@
 class Notification < ApplicationRecord
-  has_and_belongs_to_many :users 
-  has_and_belongs_to_many :groups 
-
+  has_and_belongs_to_many :users
+  has_and_belongs_to_many :groups
   belongs_to :user
-
 
   validates :title, presence: true
   validates :content, presence: true
+  validates :date, presence: true
 
 end
--- test/models/notification_test.rb
+++ test/models/notification_test.rb
@@ -1,29 +1,51 @@
 require 'test_helper'
 
 class NotificationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-def setup
-    @notification = Notification.new(title: "Example Title", first_name: "User first", last_name: "User Last", date: DateTime.now, content: "This is example content")
+
+  def setup
+    @notification = Notification.new(title: "Example Title", date: DateTime.now, content: "This is example content", user: User.new)
   end
 
-  test "should be valid" do
+  test "with title, date, content and user is valid" do
     assert @notification.valid?
   end
 
-  test "title should be present" do
-    @notification.title = "     "
-    assert_not @notification.valid?
+  test "without a title is invalid" do
+    @notification.title = ""
+    refute @notification.valid?
   end
 
-  test "first name should be present" do
-    @notification.first_name = "     "
-    assert_not @notification.valid?
+  test "without a date is invalid" do
+    @notification.date = nil
+    refute @notification.valid?
   end
 
-  test "content should be present" do
-    @notification.content = "     "
-    assert_not @notification.valid?
+  test "without a content is invalid" do
+    @notification.content = ""
+    refute @notification.valid?
   end
+
+  test "without a user is invalid" do
+    @notification.user = nil
+    refute @notification.valid?
+  end
+
+  test "title can't be blank" do
+    @notification.title = " "
+    refute @notification.valid?
+  end
+
+  test "content can't be blank" do
+    @notification.content = " "
+    refute @notification.valid?
+  end
+
+  test "has associated users" do
+    assert_respond_to @notification, :users
+  end
+
+  test "has associated groups" do
+    assert_respond_to @notification, :groups
+  end
+
 end
