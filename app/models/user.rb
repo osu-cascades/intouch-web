d2755a1bc0409333d49b00c28b6d2e7e6e3c9651
Yong Bakos <ybakos@humanoriented.com>
2018-05-09 09:22:03 -0700

    User: indent.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,25 +4,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  
-  has_and_belongs_to_many :groups  
+
+  has_and_belongs_to_many :groups
 	has_and_belongs_to_many :notifications
-  
-validates :username, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
-validates :email, uniqueness: true
-validates :first_name, presence: true,length: { maximum: 50 }
-validates :last_name, presence: true, length: { maximum: 50 }
-validates :user_type, presence: true
+
+  validates :username, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
+  validates :email, uniqueness: true
+  validates :first_name, presence: true,length: { maximum: 50 }
+  validates :last_name, presence: true, length: { maximum: 50 }
+  validates :user_type, presence: true
 
 	# Per the instructions to be able to user username as key for authentication
   #https://github.com/plataformatec/devise/wiki/How-To:-Allow-users-to-sign-in-with-something-other-than-their-email-address
-  # 
+  #
 
   def email_required?
     false
   end
 
-  
+
   # use this instead of email_changed? for rails >= 5.1
   def will_save_change_to_email?
     false
