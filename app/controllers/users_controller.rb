5c2d67de023e4a8748cbc6dab9f4cf41df9ea522
Kyleen Gonzalez <kyleen@alpineinternet.com>
2018-05-22 21:52:04 -0700

    Test: user can log in and create a new user, fix flash not showing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,8 +15,11 @@ class UsersController < ApplicationController
   	@user = User.new(user_params)
   	if @user.save
       flash[:success] = "New user created!"
+      
       redirect_to users_path
   	else
+      # flash[:error] = "New user was not created!"
+      redirect_to new_user_path, danger: "New user was not created!"
   		render 'new'
   	end
   end
--- test/system/admin_creates_user_test.rb
+++ test/system/admin_creates_user_test.rb
@@ -6,10 +6,23 @@ class AdminCreatesUserTest < ApplicationSystemTestCase
   test "admin creates a valid user" do
     sign_in users(:admin)
     visit new_user_url
+    
+    
+    fill_in "First Name", with: "Fake"
+    fill_in "Last Name", with: "Fake"
+    select('admin', :from => 'user_user_type')
+    page.check("user_group_ids_1")
+    fill_in "Username", with: "fakeuser"
+    fill_in "Email", with:  "fake@fake.com"
+    fill_in "Password", with: "fake1234"
+    fill_in "Re-type Password", with: "fake1234"
+
+    click_on "Create User"
+
+    assert_text "New user created!"
     # TODO
     # fill in the form
     # click the create button
     # assert the screen has a "success" message
   end
-
 end
