696b5304913e90158573ab74677e6cd7dcff4c65
Aaronmb123 <bakeaaro@oregonstate.edu>
2018-05-30 19:13:59 -0700

    changed mailer url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Per devise.
-  config.action_mailer.default_url_options = { host: 'abilitree-intouch-staging.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'abilitree-intouch.herokuapp.com' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
