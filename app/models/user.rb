7ad92192fd545faf0d394d0041014c05911406ce
Hanna Bennett <bennetthanna22@gmail.com>
2018-11-06 17:09:10 -0800

    Lint and add user model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,28 +1,25 @@
 class User < ApplicationRecord
-  before_save {self.username = username.downcase}
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  before_save { self.username = username.downcase }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-
   has_and_belongs_to_many :groups
-	has_and_belongs_to_many :notifications
+  has_and_belongs_to_many :notifications
 
-  validates :username, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
+  validates :username, presence: true, length: { maximum: 50 }, uniqueness: {
+    case_sensitive: false
+  }
   validates :email, uniqueness: true
-  validates :first_name, presence: true,length: { maximum: 50 }
+  validates :first_name, presence: true, length: { maximum: 50 }
   validates :last_name, presence: true, length: { maximum: 50 }
   validates :user_type, presence: true
 
-	# Per the instructions to be able to user username as key for authentication
-  #https://github.com/plataformatec/devise/wiki/How-To:-Allow-users-to-sign-in-with-something-other-than-their-email-address
-  #
+  # Per the instructions to be able to user username as key for authentication
+  # https://github.com/plataformatec/devise/wiki/How-To:-Allow-users-to-sign-in-with-something-other-than-their-email-address
 
   def email_required?
     false
   end
-
 
   # use this instead of email_changed? for rails >= 5.1
   def will_save_change_to_email?
@@ -32,5 +29,4 @@ class User < ApplicationRecord
   def active_for_authentication?
     super && !deactivated
   end
-
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,84 +1,114 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-
   def setup
-  	@user = User.new(first_name: "Tim", last_name: "Arnold", user_type: "admin", password: "timothyarnold1", username: "tarnold")
+    @user = User.new(
+      first_name: 'Tim', last_name: 'Arnold', user_type: 'admin',
+      password: 'timothyarnold1', username: 'tarnold'
+    )
   end
 
-  test "should be valid" do
-  	assert users(:admin).valid?
+  test 'user should be valid when first_name, last_name, user_type, password,
+        and username are present' do
+    assert users(:admin).valid?
   end
 
-  # presense
-
-  test "first name should be present" do
-  	@user.first_name = "   "
-  	assert_not @user.valid?
-  end
-
-  test "lastName should be present" do
-  	@user.last_name = "   "
-  	assert_not @user.valid?
-  end
-
-  test "usertype should be present" do
-    @user.user_type = "  "
+  test 'user should be invalid when first_name is empty' do
+    @user.first_name = '   '
     assert_not @user.valid?
   end
 
-  test "password should be present" do
-    @user.password = "   "
+  test 'user should be invalid when last_name is empty' do
+    @user.last_name = '   '
     assert_not @user.valid?
   end
 
-  test "username should be present" do
-    @user.username = "   "
+  test 'user should be invalid when user_type is empty' do
+    @user.user_type = '    '
     assert_not @user.valid?
   end
 
-  # length validation
-
-  test "firstName should not be too long" do
-    @user.first_name = "a" * 51
+  test 'user should be invalid when password is empty' do
+    @user.password = '    '
     assert_not @user.valid?
   end
 
-  test "lastName should not be too long" do
-    @user.last_name = "a" * 51
+  test 'user should be invalid when username is empty' do
+    @user.username = '   '
     assert_not @user.valid?
   end
 
-  test "username should not be too long" do
-    @user.username = "a" * 51
+  test 'user should be invalid when first_name is not present' do
+    @user.first_name = nil
     assert_not @user.valid?
   end
 
-  test "password should not be less than 6 characters" do
-    @user.password = "a" * 5
+  test 'user should be invalid when last_name is not present' do
+    @user.last_name = nil
     assert_not @user.valid?
   end
 
-  test "username should be unique" do
-  	duplicate_user = @user.dup
-    duplicate_user.username = @user.username
-  	@user.save
-  	assert_not duplicate_user.valid?
+  test 'user should be invalid when user_type is not present' do
+    @user.user_type = nil
+    assert_not @user.valid?
   end
 
-  test "username should be saved as lower case" do
-    mixed_case_username = "UseRNaMe"
+  test 'user should be invalid when password is not present' do
+    @user.password = nil
+    assert_not @user.valid?
+  end
+
+  test 'user should be invalid when username is not present' do
+    @user.username = nil
+    assert_not @user.valid?
+  end
+
+  test 'first_name should not be longer than 50 characters' do
+    @user.first_name = 'a' * 51
+    assert_not @user.valid?
+  end
+
+  test 'last_name should not be longer than 50 characters' do
+    @user.last_name = 'a' * 51
+    assert_not @user.valid?
+  end
+
+  test 'username should not be longer than 50 characters' do
+    @user.username = 'a' * 51
+    assert_not @user.valid?
+  end
+
+  test 'password should not be less than 6 characters' do
+    @user.password = 'a' * 5
+    assert_not @user.valid?
+  end
+
+  test 'username should be unique' do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test 'username should be saved as lower case' do
+    mixed_case_username = 'UseRNaMe'
     @user.username = mixed_case_username
     @user.save
     assert_equal mixed_case_username.downcase, @user.reload.username
   end
 
-  test "userType should be admin staff or client" do
-    user_types = ['admin', 'staff', 'client']
-    @user.user_type = 'cookie monster'
-    assert_not(user_types.include? @user.user_type)
+  test 'email should be unique' do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
   end
 
+  test 'has and belongs to many groups' do
+    association = User.reflect_on_association(:groups)
+    assert_same association.macro, :has_and_belongs_to_many
+  end
 
-
+  test 'has and belongs to many notifications' do
+    association = User.reflect_on_association(:notifications)
+    assert_same association.macro, :has_and_belongs_to_many
+  end
 end
