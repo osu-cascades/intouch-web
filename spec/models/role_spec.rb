320ff238e2d1238d549de164c9a6829e837b166b
Hanna Bennett <bennetthanna22@gmail.com>
2018-11-06 20:21:58 -0800

    Fix role model tests

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,24 +1,38 @@
-require 'test_helper'
+require 'rails_helper'
 
-class RoleTest < ActiveSupport::TestCase
-
- def setup
-    @role = Role.new(name: "Staff")
+RSpec.describe Role, type: :model do
+  before(:each) do
+    @role = Role.new(name: 'Role')
   end
 
-  test "should be valid" do
-    assert @role.valid?
+  it 'is valid when name is set' do
+    expect(@role).to be_valid
   end
 
-  test "role name should be present" do
-    @role.name = "     "
-    assert_not @role.valid?
+  it 'is invalid when name is blank' do
+    @role.name = '    '
+    expect(@role).to_not be_valid
   end
 
-  test "role name should not be too long" do
-    @role.name = "a" * 101
-    assert_not @role.valid?
+  it 'is invalid when name is longer than 100 characters' do
+    @role.name = 'a' * 101
+    expect(@role).to_not be_valid
   end
 
+  it 'is invalid when name is not unique' do
+    @role.save
+    duplicate_role = @role.dup
+    expect(duplicate_role).to_not be_valid
+  end
 
+  it 'is invalid when the only thing unique about name is case' do
+    @role.save
+    duplicate_role = @role.dup
+    duplicate_role.name = @role.name.upcase
+    expect(duplicate_role).to_not be_valid
+  end
+
+  it { should validate_presence_of :name }
+  it { should validate_length_of(:name).is_at_most(100) }
+  it { should validate_uniqueness_of(:name).case_insensitive }
 end
