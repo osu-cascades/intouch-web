283d605dc21c36c5fb4a24a204601f1d6c3021be
Hanna Bennett <bennetthanna22@gmail.com>
2018-11-06 17:05:12 -0800

    Lint and add notification model tests

--- test/models/notification_test.rb
+++ test/models/notification_test.rb
@@ -1,51 +1,65 @@
 require 'test_helper'
 
 class NotificationTest < ActiveSupport::TestCase
-
   def setup
-    @notification = Notification.new(title: "Example Title", date: DateTime.now, content: "This is example content", user: User.new)
+    @notification = Notification.new(
+      title: 'Example Title', date: DateTime.now,
+      content: 'This is example content', user: User.new
+    )
   end
 
-  test "with title, date, content and user is valid" do
+  test 'notification should be valid when title, date, content and user are
+        present' do
     assert @notification.valid?
   end
 
-  test "without a title is invalid" do
-    @notification.title = ""
+  test 'notification should be invalid when title is not present' do
+    @notification.title = nil
     refute @notification.valid?
   end
 
-  test "without a date is invalid" do
+  test 'notification should be invalid when date is not present' do
     @notification.date = nil
     refute @notification.valid?
   end
 
-  test "without a content is invalid" do
-    @notification.content = ""
+  test 'notification should be invalid when content is not present' do
+    @notification.content = nil
     refute @notification.valid?
   end
 
-  test "without a user is invalid" do
+  test 'notification should be invalid when user is not present' do
     @notification.user = nil
     refute @notification.valid?
   end
 
-  test "title can't be blank" do
-    @notification.title = " "
+  test 'notification should be invalid when title is blank' do
+    @notification.title = '    '
     refute @notification.valid?
   end
 
-  test "content can't be blank" do
-    @notification.content = " "
+  test 'notification should be invalid when date is blank' do
+    @notification.date = '    '
     refute @notification.valid?
   end
 
-  test "has associated users" do
-    assert_respond_to @notification, :users
+  test 'notification should be invalid when content is blank' do
+    @notification.content = '    '
+    refute @notification.valid?
   end
 
-  test "has associated groups" do
-    assert_respond_to @notification, :groups
+  test 'has and belongs to many groups' do
+    association = Notification.reflect_on_association(:groups)
+    assert_same association.macro, :has_and_belongs_to_many
   end
 
+  test 'has and belongs to many users' do
+    association = Notification.reflect_on_association(:users)
+    assert_same association.macro, :has_and_belongs_to_many
+  end
+
+  test 'belongs to a user' do
+    association = Notification.reflect_on_association(:user)
+    assert_same association.macro, :belongs_to
+  end
 end
