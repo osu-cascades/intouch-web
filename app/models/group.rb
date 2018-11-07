467e7ce6b3d5b10adc3ea96fbe8059a8f4123413
Hanna Bennett <bennetthanna22@gmail.com>
2018-11-06 17:15:55 -0800

    Lint and add group model tests

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,8 +1,8 @@
 class Group < ApplicationRecord
-
   has_and_belongs_to_many :users
   has_and_belongs_to_many :notifications
 
-  validates :name, presence: true, length: { maximum: 100 }, uniqueness: { case_sensitive: false }
-
+  validates :name, presence: true, length: { maximum: 100 }, uniqueness: {
+    case_sensitive: false
+  }
 end
