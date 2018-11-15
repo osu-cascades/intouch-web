b05f59b489314ffbf0aa1d1c2b606234f129df70
Hanna Bennett <bennetthanna22@gmail.com>
2018-11-14 20:09:33 -0800

    Make all tests green

--- spec/views/users/edit.html.erb_spec.rb
+++ spec/views/users/edit.html.erb_spec.rb
@@ -2,13 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "users/edit", type: :view do
   before(:each) do
-    @user = assign(:user, User.create!())
+    @user = assign(:user, User.create!(first_name: 'First',
+      last_name: 'Last',
+      user_type: 'admin',
+      password: 'password',
+      username: 'testuser'))
   end
 
-  skip "renders the edit user form" do
+  it "renders the edit user form" do
     render
 
-    assert_select "form[action=?][method=?]", user_path(@user), "post" do
-    end
+    assert_select "form[action=?][method=?]", user_path(@user), "post"
   end
 end
