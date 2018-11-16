984c1b8e4839d2fa888afa1d1b966db40a8e2763
Camron Fritz <camfritz@gmail.com>
2018-11-16 12:12:42 -0800

    Add notifications controller tests for redirect to new user session.

--- spec/controllers/notifications_controller_spec.rb
+++ spec/controllers/notifications_controller_spec.rb
@@ -1,5 +1,56 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe NotificationsController, type: :controller do
-#   include Devise::Test::ControllerHelpers
-# end
+RSpec.describe NotificationsController, type: :controller do
+  include Devise::Test::ControllerHelpers
+
+let(:valid_attributes) do
+{
+	title: 'Test',
+	content: 'Test123',
+	date: Time.now
+}
+end
+
+  describe 'GET #index' do
+    it 'returns to new user session' do
+      get :index, params: {}
+      expect(response).to redirect_to(new_user_session_url)
+    end
+  end
+
+  describe 'GET #show' do
+  	it 'redirects to new user session' do
+  		get :show, params: {id: 1}
+  		expect(response).to redirect_to(new_user_session_url)
+  	end
+  end
+
+  describe 'GET #new' do
+  	it 'redirects to a new user session' do
+  		get :new, params: {}
+  		expect(response).to redirect_to(new_user_session_url)
+  	end
+  end
+
+    describe 'GET #edit' do
+  	it 'redirects to a new user session' do
+  		get :edit, params: {id: 1}
+  		expect(response).to redirect_to(new_user_session_url)
+  	end
+  end
+
+    describe 'POST #create' do
+  	it 'redirects to a new user session' do
+  		post :create, params: {}
+  		expect(response).to redirect_to(new_user_session_url)
+  	end
+  end
+
+    describe 'DELETE #destroy' do
+  	it 'redirects to a new user session' do
+  		delete :destroy, params: {id: 1}
+  		expect(response).to redirect_to(new_user_session_url)
+  	end
+  end
+
+end
