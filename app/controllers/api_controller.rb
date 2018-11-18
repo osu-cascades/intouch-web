18879eaa4b3ac0dd5a3d543d16e50b6b5cf62e60
Brad Cook <cookbrad@oregonstate.edu>
2018-11-18 14:37:17 -0800

    change @user to user

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -116,10 +116,10 @@ class ApiController < ApplicationController
     user = User.find_for_authentication(username: username)
     if user && user.valid_password?(password)
       @notification = Notification.new
-      @notification.title = "Reply from: #{@user.first_name} #{@user.last_name}"
+      @notification.title = "Reply from: #{user.first_name} #{user.last_name}"
       @notification.content = content
       @notification.date = DateTime.now
-      @notification.user_id = @user.id
+      @notification.user_id = user.id
       send_to_mobile(sender)
     end
   end
