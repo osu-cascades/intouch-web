a0213d839d758f6a488d78b50446863405694d58
Camron Fritz <camfritz@gmail.com>
2019-04-19 10:58:22 -0700

    Add basic event CRUD functionality.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,8 +8,37 @@ class EventsController < ApplicationController
   end
 
   def new
+  	@event = Event.new
   end
 
   def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    @event.update(event_params)
+
+    redirect_to event_path(@event)
+   end
+
+
+  def create
+  	event = Event.create(event_params)
+
+  	redirect_to events_path
+  end
+
+ 	def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+
+    redirect_to events_path
+  end
+
+  private
+
+  def event_params
+  	params.require(:event).permit(:title, :description, :time, :place, :notes, :group_participants, :hosted_by)
   end
 end
