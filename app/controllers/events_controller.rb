ee155e9e0352176542c322cc4e80d435848e603e
Hanna Bennett <bennetthanna22@gmail.com>
2019-04-28 22:49:28 -0700

    Implement event/user relationship on event creation

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -26,10 +26,20 @@ class EventsController < ApplicationController
   def create
     @event = Event.new(event_params)
     @groups = Group.where(id: params[:event][:group_participants])
-    @event.group_participants = nil
+    group_participants = []
+    user_recipients = []
     @groups.each do |group|
       @event.groups << group
-      @event.group_participants << group.name
+      group_participants << group.name
+      @users = group.users
+      @users.each do |user|
+        user_recipients << user
+      end
+    end
+    @event.group_participants = group_participants.join(', ')
+    user_recipients.uniq!(&:username)
+    user_recipients.each do |user|
+      @event.users << user
     end
 
     if @event.save
